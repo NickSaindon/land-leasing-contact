953b738bbed4e2a6bc927e2ad4ff77a87bd42d25
Nick Saindon <ntimeproductions@gmail.com>
2022-05-30 20:54:27 -0400

    Added context api, custom hook, and message template

---
+++ components/ToastContainer.js
@@ -0,0 +1,19 @@
+import Toast from '../components/Toast';
+import { useToastStateContext } from '../context/ToastContext';
+
+export default function ToastContainer() {
+  const { toasts } = useToastStateContext();
+  return (
+    <div className="toast-container">
+    {toasts &&
+        toasts.map((toast) => (
+        <Toast
+            id={toast.id}
+            key={toast.id}
+            type={toast.type}
+            message={toast.message}
+        />
+        ))}
+    </div>
+  );
+}
